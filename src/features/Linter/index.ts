aac6601beb13e9b44b097a05da7d8d315741811a
Michael Naumov <mnaoumov@gmail.com>
2026-03-08 02:14:13 -0600

    fix: make eslint the default linter option

--- src/features/Linter/index.ts
+++ src/features/Linter/index.ts
@@ -5,7 +5,7 @@ import { Biome } from './Biome.ts';
 import { Eslint } from './Eslint.ts';
 import { None } from './None.ts';
 
-export const LINTER_OPTIONS: readonly FeatureOption[] = [new None(), new Biome(), new Eslint()];
+export const LINTER_OPTIONS: readonly FeatureOption[] = [new Eslint(), new Biome(), new None()];
 
 export async function promptLinter(defaultValue?: string): Promise<string> {
   return promptFeature(LINTER_OPTIONS, 'Linter', defaultValue);
